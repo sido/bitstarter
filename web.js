55804d8db83ac95903fe2089aefa0591dd28c4d8
Sido van Gennip <sido@angelmechanics.com>
2013-07-06 17:00:07 +0200

    Modified web.js again, except this time have it read "index.html" from disk and use that data in place of "Hello World 2!".

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var page = fs.readFileSync(__dirname + '/index.html');
+  response.end(page.toString());
 });
 
 var port = process.env.PORT || 5000;
