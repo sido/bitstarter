c0a42e5de6eeba43eff54a25cd669259634a0665
Sido van Gennip <sido@angelmechanics.com>
2013-08-05 17:54:07 +0200

    Code formatting of web.js

--- web.js
+++ web.js
@@ -4,16 +4,16 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 // all environments
-app.configure(function(){
-  app.use(express.static(__dirname + '/public'));
+app.configure(function () {
+	app.use(express.static(__dirname + '/public'));
 })
 
-app.get('/', function(request, response) {
-  var page = fs.readFileSync(__dirname + '/index.html');
-  response.end(page.toString());
+app.get('/', function (request, response) {
+	var page = fs.readFileSync(dirname + '/index.html');
+	response.end(page.toString());
 });
 
 var port = process.env.PORT || 8080;
-app.listen(port, function() {
-  console.log("Listening on " + port);
+app.listen(port, function () {
+	console.log("Listening on " + port);
 });
